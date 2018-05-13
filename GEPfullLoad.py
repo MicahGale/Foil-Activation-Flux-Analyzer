68daf2be9eab8e2a29ce016bff34289bfb996e1d
Micah Gale <mgale@mit.edu>
2018-05-13 17:09:43 -0400

    Made axial plots

--- GEPfullLoad.py
+++ GEPfullLoad.py
@@ -19,5 +19,30 @@ rad.tick_params(labelcolor='w', top=False, bottom=False, left=False,right=False)
 
 font={'size':20}
 data.plotRadial(5,ax5,None,font,False,False,False,'Layer 5')
-rad.set_ylabel("Uncorrect Specific ReactionRate\n($\\eta\\phi\\Sigma_c/\\rho$)[$s^{-1    }g^{-1}]$", **font)
+rad.set_ylabel("Uncorrected Specific Reaction Rate\n($\\eta\\phi\\Sigma_c/\\rho$)[$s^{-1    }g^{-1}]$", **font)
 data.plotRadial(1,ax1,'radial',font,True,True,False,'Layer 1')
+
+fig.clf()
+fig.set_size_inches(16,9)
+ax=fig.add_subplot(111)
+axLog=fig.add_subplot(2,1,1)
+axLin=fig.add_subplot(2,1,2)
+
+
+# Turn off axis lines and ticks of the big subplot
+ax.spines['top'].set_color('none')
+ax.spines['bottom'].set_color('none')
+ax.spines['left'].set_color('none')
+ax.spines['right'].set_color('none')
+ax.tick_params(labelcolor='w', top=False, bottom=False,
+              left=False,right=False)
+
+
+
+data.plotAxial(7,axLog,None,font,False,False,False,True)
+
+ax.set_ylabel("Uncorrected Specific Reaction Rate\n($\\eta\\phi\\Sigma_c/\\rho$)[$s^{-1    }g^{-1}]    $",**font)
+
+data.plotAxial(7,axLin,'axial',font,save=True,yAxisLabel=False,
+        xAxisLabel=True,logLog=False)
+
